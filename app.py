3ea0a10fe548e244e538ba8c31aa730f7517ebcc
danerose000-crypto <danerose000@gmail.com>
2025-11-20 16:34:25 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Insurance Quote – Test", layout="centered")
+
+st.title("Insurance Quote App (Test)")
+st.write("If you can see this on Streamlit Cloud, everything is wired up correctly!")
